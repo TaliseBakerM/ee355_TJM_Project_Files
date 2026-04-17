e59ea243772823fbb355ad7ab36f580280c9a6b9
May Syi Ang <maysyian@usc.edu>
2026-04-16 17:46:21 -0700

    added preliminary codeName() function. Need to test

--- misc.cpp
+++ misc.cpp
@@ -1,5 +1,6 @@
 
 #include "misc.h"
+#include <cctype>
 
 void printMe(string type){
     if (type == "banner"){
@@ -13,4 +14,17 @@ void printMe(string type){
      ##    ##     ##  #######   ######  ##     ## ##    ##         ########   #######   #######  ##    ##\n";
         cout << endl << banner << endl << endl;
     }
+}
+
+string codeName(string str1, string str2){
+    string ID = "";
+    for (int i = 0; i < str1.length(); i++) {
+        if (str1[i] != ' ' && str1[i] != '-')
+            ID += tolower(str1[i]);
+    }
+    for (int i = 0; i < str2.length(); i++) {
+        if (str2[i] != ' ' && str2[i] != '-')
+            ID += tolower(str2[i]);
+    }
+    return ID;
 }
